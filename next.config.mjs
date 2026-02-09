56043c6923f3339b86ace1eb38daaacdb2b3a5d9
huda-7077 <85928499+huda-7077@users.noreply.github.com>
2026-02-09 15:28:38 +0700

    chore(deps): upgrade Next.js from 14.2.35 to 16.1.6

--- next.config.mjs
+++ next.config.mjs
@@ -12,13 +12,7 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.ttf$/i,
-      type: "asset/resource",
-    });
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
